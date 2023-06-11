38dc874001a5326395da40a7732ccec5f4466f2e
mustofa adny <adniadni04@gmail.com>
2023-06-11 20:01:52 +0700

    update time session

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(
     secret: "keyboard cat",
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 60000 },
+    cookie: { maxAge: 600000 },
   })
 );
 app.use(flash());
